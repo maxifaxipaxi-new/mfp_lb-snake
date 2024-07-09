1ddf2017190e1b6bf91c842391652628a51c0405
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2024-07-09 15:32:37 +0200

    Add files via upload

---
+++ mfp_lb-snake/client.lua
@@ -0,0 +1,62 @@
+local identifier = "mfp-lb-snake"
+
+CreateThread(function ()
+    while GetResourceState("lb-phone") ~= "started" do
+        Wait(500)
+    end
+
+    local function AddApp()
+        local added, errorMessage = exports["lb-phone"]:AddCustomApp({
+            identifier = identifier,
+            name = Config.name,
+            description = Config.description,
+            developer = "MFPSCRIPTS.com", -- please leave this, it would be fair for a free app (-;
+            defaultApp = Config.defaultApp, 
+            size = Config.size, 
+            price = Config.price, 
+            images = {"https://r2.fivemanage.com/pub/2rbzvo4py5mz.png", "https://r2.fivemanage.com/pub/di22j5sc97su.png", "https://r2.fivemanage.com/pub/8sbjfr6gc77q.png"}, -- OPTIONAL array of images for the app on the app store
+            ui = GetCurrentResourceName() .. "/ui/index.html", 
+            icon = "https://cfx-nui-" .. GetCurrentResourceName() .. "/ui/assets/icon.png"
+        })
+
+        if not added then
+            print("Could not add app:", errorMessage)
+        end
+    end
+
+    AddApp()
+
+    AddEventHandler("onResourceStart", function(resource)
+        if resource == "lb-phone" then
+            AddApp()
+        end
+    end)
+end)
+
+
+
+
+
+
+
+
+---   ADS - ADS - ADS - ADS  ---
+--- can be removed, just ads: ---
+
+AddEventHandler('onResourceStart', function(resourceName)
+   if (GetCurrentResourceName() ~= resourceName) then
+     return
+   end
+   print(" _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ ")
+   print("|     |   __|  _  |   __|     | __  |     |  _  |_   _|   __|")
+   print("| | | |   __|   __|__   |   --|    -|-   -|   __| | | |__   |")
+   print("|_|_|_|__|  |__|  |_____|_____|__|__|_____|__|    |_| |_____|")
+   print("The resource " .. resourceName .. " has been started")
+ end)
+ 
+ AddEventHandler('onResourceStop', function(resourceName)
+   if (GetCurrentResourceName() ~= resourceName) then
+     return
+   end
+   print('The resource ' .. resourceName .. ' was stopped. Created by MFPSCRIPTS.com!')
+ end)
---
+++ mfp_lb-snake/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.name = "Classic Snake" -- Name of the App
+Config.description = "Relive the Retro Fun!" -- disc in the appstore (edit for your local)
+Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
+Config.size = 3932 -- OPTIONAL in kb
+Config.price = 0 -- OPTIONAL, Make players pay with in-game money to download the app
+
+
+-- Support on discord.mfpscripts.com if needed. Check out issues on github. --
